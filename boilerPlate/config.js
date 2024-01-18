7ef2cfd48a630ac7d4fd389ad648f38096811474
BDhanush489 <bdhanush489@gmail.com>
2024-01-18 19:26:52 +0530

    Dhanush

--- boilerPlate/config.js
+++ boilerPlate/config.js
@@ -1,62 +1,59 @@
-import express from 'express';
-import fs from 'fs';
+import express from "express";
+import fs from "fs";
 
-import myJson from '../src/Config.json' assert {type: 'json'};
+import { StartFunc as StartFuncSeq } from "../src/ksSample/kSequelize/Config/TableNames.js";
+import myJson from "../src/Config.json" assert { type: "json" };
 
 var router = express.Router();
 
-router.get('/files', (req, res) => {    
-    if(myJson.isSequelize){
-        let LocalTablesArray= LocalFuncForSequelize();
-        res.end();
-        return  
-    };
+router.get("/files", async (req, res) => {
+  if (myJson.isSequelize) {
+    let LocalTablesArray = await StartFuncSeq();
+    // let NewLocalTablesArray=LocalTablesArray.find(obj => obj.name);;
+    res.json(LocalTablesArray);
+    return;
+  }
 
-    let LocalFilesArray = LocalFuncForFiles();
+  let LocalFilesArray = LocalFuncForFiles();
 
-    res.json(LocalFilesArray);
+  res.json(LocalFilesArray);
 });
 
-router.get('/dataSource', (req, res) => {
-    let mySequelizeJsonData = {};
-    mySequelizeJsonData.isSequelize = myJson.isSequelize;
+router.get("/dataSource", (req, res) => {
+  let mySequelizeJsonData = {};
+  mySequelizeJsonData.isSequelize = myJson.isSequelize;
 
-    res.json(mySequelizeJsonData);
+  res.json(mySequelizeJsonData);
 });
 
 let LocalFuncForFiles = () => {
+  let LocalFilesPath = "KData/JSON/316";
+  let CommonFiles = fs.readdirSync(LocalFilesPath);
+  let LocalFilesArray = [];
 
-    let LocalFilesPath = "KData/JSON/316";
-    let CommonFiles = fs.readdirSync(LocalFilesPath);
-    let LocalFilesArray = [];
+  CommonFiles.forEach(function (file, index) {
+    let result = file.endsWith(".json");
+    if (result === true) {
+      LocalFilesArray.push(file);
+    }
+  });
 
-    CommonFiles.forEach(function (file, index) {
-        let result = file.endsWith(".json");
-        if (result === true){
-            LocalFilesArray.push(file);
-        }
-    });
-
-    return LocalFilesArray;
-
+  return LocalFilesArray;
 };
 
 let LocalFuncForSequelize = () => {
+  let LocalFilesPath = "KData/JSON/316";
+  let CommonFiles = fs.readdirSync(LocalFilesPath);
+  let LocalFilesArray = [];
 
-    let LocalFilesPath = "KData/JSON/316";
-    let CommonFiles = fs.readdirSync(LocalFilesPath);
-    let LocalFilesArray = [];
+  CommonFiles.forEach(function (file, index) {
+    let result = file.endsWith(".json");
+    if (result === true) {
+      LocalFilesArray.push(file);
+    }
+  });
 
-    CommonFiles.forEach(function (file, index) {
-        let result = file.endsWith(".json");
-        if (result === true){
-            LocalFilesArray.push(file);
-        }
-    });
-
-    return LocalFilesArray;
-
+  return LocalFilesArray;
 };
-
 
 export { router };
