39841849ec7b3e932dd5a34d56e9eb11bb8d7b6f
Keshav <nknnkumar@yahoo.com>
2024-01-18 23:11:09 +0530

    keshav

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 var router = express.Router();
 
-import { router as routerFromksSample } from './ksSample/route.js';
 import { router as routerKSequelize } from './kSequelize/route.js';
 
+import { router as routerFromksSample } from './ksSample/route.js';
+
 router.use('/tableNames', routerKSequelize);
+
 router.use('/ksSample', routerFromksSample);
 
 export { router };
