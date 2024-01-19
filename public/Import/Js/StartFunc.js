7c1ac80b2b1e7b1597fe7d08537485b192766d2f
Keshav <nknnkumar@yahoo.com>
2024-01-19 18:56:21 +0530

    keshav

--- public/Import/Js/StartFunc.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncNavMenu } from "./NavMenu/EntryFile.js";
-import { StartFunc as StartFuncAddlisteners } from "./Addlisteners/StartFunc.js";
-
-const StartFunc = () => {
-    StartFuncNavMenu();
-    StartFuncAddlisteners();
-};
-
-StartFunc();
