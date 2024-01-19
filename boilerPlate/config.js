acf30490d6445fd0188c7938143377209bb19648
BDhanush489 <bdhanush489@gmail.com>
2024-01-19 21:27:41 +0530

    Dhanush

--- boilerPlate/config.js
+++ boilerPlate/config.js
@@ -10,42 +10,40 @@ import myJson from "../src/Config.json" assert { type: "json" };
 var router = express.Router();
 
 router.get("/files", async (req, res) => {
-    if (myJson.isSequelize) {
-        let LocalTablesArray = await StartFuncSeq();
-        
-        let LocalReturnArray = LocalTablesArray.map(function (element) {
-            return element.name;
-        });
+  if (myJson.isSequelize) {
+    let LocalTablesArray = await StartFuncSeq();
 
-        res.json(LocalReturnArray);
-        return;
-    }
+    let LocalReturnArray = LocalTablesArray.map((element) => element.name);
 
-    let LocalFilesArray = LocalFuncForFiles();
+    res.json(LocalReturnArray);
+    return;
+  }
 
-    res.json(LocalFilesArray);
+  let LocalFilesArray = LocalFuncForFiles();
+
+  res.json(LocalFilesArray);
 });
 
 router.get("/dataSource", (req, res) => {
-    let mySequelizeJsonData = {};
-    mySequelizeJsonData.isSequelize = myJson.isSequelize;
+  let mySequelizeJsonData = {};
+  mySequelizeJsonData.isSequelize = myJson.isSequelize;
 
-    res.json(mySequelizeJsonData);
+  res.json(mySequelizeJsonData);
 });
 
 let LocalFuncForFiles = () => {
-    let LocalFilesPath = "KData/JSON/316";
-    let CommonFiles = fs.readdirSync(LocalFilesPath);
-    let LocalFilesArray = [];
+  let LocalFilesPath = "KData/JSON/316";
+  let CommonFiles = fs.readdirSync(LocalFilesPath);
+  let LocalFilesArray = [];
 
-    CommonFiles.forEach(function (file, index) {
-        let result = file.endsWith(".json");
-        if (result === true) {
-            LocalFilesArray.push(file);
-        }
-    });
+  CommonFiles.forEach(function (file, index) {
+    let result = file.endsWith(".json");
+    if (result === true) {
+      LocalFilesArray.push(file);
+    }
+  });
 
-    return LocalFilesArray;
+  return LocalFilesArray;
 };
 
 export { router };
