3a27b51b55126d3f4992f459749606f7ab8a1fce
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-19 13:46:28 +0530

    SREENIVAS

--- public/src/BuildTh/Delete/js/StartFunc.js
+++ public/src/BuildTh/Delete/js/StartFunc.js
@@ -1,12 +1,7 @@
-// import { StartFunc as StartFuncNavMenu } from "./NavMenu/EntryFile.js";
-import { StartFunc as StartFuncformLoad } from "./formLoad/pullTableData/ButtonClickFunc.js";
-// import { StartFunc as StartFuncAddlisteners } from "./Addlisteners/StartFunc.js";
+import { StartFunc as StartFuncformLoad } from "./formLoad/EntryFile.js";
 
 const StartFunc = () => {
-    // StartFuncNavMenu();
-    StartFuncformLoad().then(() => {
-        // StartFuncAddlisteners();
-    });
+    StartFuncformLoad();
 };
 
 StartFunc();
