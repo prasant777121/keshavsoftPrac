3e7d7dc5ff7888a841e1f332de1adbae8e8ba9cf
Keshav <nknnkumar@yahoo.com>
2024-01-19 22:04:38 +0530

    keshav

--- PortListen.js
+++ PortListen.js
@@ -2,6 +2,10 @@ let StartFunc = () => {
     console.log(`Listening in some local port : 7016`);
     console.log(`http server at http://localhost:7016`);
 
+    if ("KS_SQLITE_PASSWORD" in process.env === false) {
+        console.log("KS_SQLITE_PASSWORD not found in .env file")
+    };
+
     process.env.UUID = uuidv4();
 };
 
