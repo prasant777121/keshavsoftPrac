7e11bfb330dbc4d2c0d7c1fc5722a9709db37b60
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-20 18:44:22 +0530

    SREENIVAS

---
+++ src/ksSample/kSequelize/ReadFileList/readFileFromId.js
@@ -0,0 +1,40 @@
+import { StartFunc as StartFuncInitializeSequelizeWithTableName } from '../modals/initializeSequelizeWithTableName.js';
+
+let StartFunc = async ({ inId }) => {
+    try {
+        const LocalTableData = await StartFuncInitializeSequelizeWithTableName();
+
+        const users = await LocalTableData.findAll();
+
+        const records = users.map(function (result) {
+            return result.dataValues
+        });
+
+        const user = records.find((user) => user.id == inId);
+
+        return await user;
+    } catch (error) {
+        return await error;
+    };
+};
+
+export { StartFunc };
+
+// let StartFunc = async () => {
+//     try {
+//         const sequelize = StartFuncInitializeSequelize();
+
+//         const Tickets = sequelize.define('sample', dataColumns, { freezeTableName: true }
+//         );
+
+//         const users = await Tickets.findAll();
+
+//         const records = users.map(function (result) {
+//             return result.dataValues
+//         });
+
+//         return await records;
+//     } catch (error) {
+//         return await error;
+//     };
+// };
