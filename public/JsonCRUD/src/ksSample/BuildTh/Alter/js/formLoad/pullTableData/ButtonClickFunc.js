80ee0e794a4ca5732b46052389a7f1586cbbdb53
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-20 19:43:21 +0530

    SREENIVAS

--- public/JsonCRUD/src/ksSample/BuildTh/Alter/js/formLoad/pullTableData/ButtonClickFunc.js
+++ public/JsonCRUD/src/ksSample/BuildTh/Alter/js/formLoad/pullTableData/ButtonClickFunc.js
@@ -1,16 +1,8 @@
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch.js";
+import { StartFunc as StartFuncFetchFunc } from "./FetchFunc.js";
 
 let StartFunc = async () => {
-    let jVarLocalFetchData = await LocalFetchFiles();
-    StartFuncAfterFetch({ inFromFetch: jVarLocalFetchData.JsonData });
+    let jVarLocalFetchData = await StartFuncFetchFunc();
+    StartFuncAfterFetch({ inFromFetch: jVarLocalFetchData });
 };
-
-let LocalFetchFiles = async () => {
-    let jVarLocalFetchUrl = "/src/ksSample/DataOnly";
-    let response = await fetch(jVarLocalFetchUrl);
-    let data = await response.json();
-
-    return data;
-};
-
 export { StartFunc };
