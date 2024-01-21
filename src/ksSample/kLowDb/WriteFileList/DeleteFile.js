e50d82257b925d7ec88102c9b6c85b18a55559c5
BDhanush489 <bdhanush489@gmail.com>
2024-01-21 16:28:44 +0530

    Dhanush

--- src/ksSample/kLowDb/WriteFileList/DeleteFile.js
+++ src/ksSample/kLowDb/WriteFileList/DeleteFile.js
@@ -1,34 +1,44 @@
-import { LowSync } from 'lowdb'
-import { JSONFileSync } from 'lowdb/node'
-import Configjson from '../../../Config.json' assert { type: 'json' };
-import fileNameJson from '../fileName.json' assert { type: 'json' };
+import { LowSync } from "lowdb";
+import { JSONFileSync } from "lowdb/node";
+import Configjson from "../../../Config.json" assert { type: "json" };
+import fileNameJson from "../fileName.json" assert { type: "json" };
 
-let StartFunc = ({ inId }) => {
-    let LocalId = inId;
-    let UserDataFilePath = `${Configjson.JsonPath}/${fileNameJson.fileName}`;
-    const defaultData = { error: "From KLowDb" }
+let StartFunc = async ({ inId }) => {
+  let LocalId = inId;
+  let UserDataFilePath = `${Configjson.JsonPath}/${fileNameJson.fileName}`;
+  const defaultData = { error: "From KLowDb" };
 
+  const db = new LowSync(new JSONFileSync(UserDataFilePath), defaultData);
+  db.read();
+  let LocalarrayOfObjects = db.data;
 
-    const db = new LowSync(new JSONFileSync(UserDataFilePath), defaultData);
-    db.read();
-    let LocalarrayOfObjects = db.data;
+  const LocalFindId = LocalarrayOfObjects.find((obj) => obj.UuId === LocalId);
 
-    let LocalArrayAfterDelete = deleteObjectById({ inCollection: LocalarrayOfObjects, inId: LocalId });
+  if (LocalFindId === undefined) {
+    return await { KTF: false, KReason: "Id not found in data" };
+  }
 
-    db.data = LocalArrayAfterDelete;
-    db.write();
+  let LocalArrayAfterDelete = deleteObjectById({
+    inCollection: LocalarrayOfObjects,
+    inId: LocalId,
+  });
 
-    return true;
+  db.data = LocalArrayAfterDelete;
+  db.write();
+
+  return await true;
 };
 
 let deleteObjectById = ({ inCollection, inId }) => {
-    let LocalCollection = inCollection;
-    let LocalId = inId;
+  let LocalCollection = inCollection;
+  let LocalId = inId;
 
-    LocalCollection.splice(LocalCollection.findIndex(a => a.UuId === LocalId), 1)
+  LocalCollection.splice(
+    LocalCollection.findIndex((a) => a.UuId === LocalId),
+    1
+  );
 
-    return LocalCollection;
+  return LocalCollection;
 };
-
 
 export { StartFunc };
