3387fd6dd57111e9901ba67554574f7f3f0576f0
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-01-22 20:47:48 +0530

    keshav

--- public/JsonCRUD/src/ksSample/BuildTh/CreateNewMenu/CreateNew/js/Addlisteners/SaveButtonClickId/FetchFromFuncs/FetchHeaders/EntryFile.js
+++ public/JsonCRUD/src/ksSample/BuildTh/CreateNewMenu/CreateNew/js/Addlisteners/SaveButtonClickId/FetchFromFuncs/FetchHeaders/EntryFile.js
@@ -1,15 +1,29 @@
 import KeysJson from './Keys.json' assert {type: 'json'};
 
 let StartFunc = () => {
-    let jVarLocalBodyKeys = {};
-    let LocalFoam = document.getElementById("FormId");
-    let LocalNerFormData = new FormData(LocalFoam);
+    let jVarLocalForm = document.getElementById("FormId");
 
-    jVarLocalBodyKeys.Name = LocalNerFormData.get("UserName");
-
-    KeysJson.body = JSON.stringify(jVarLocalBodyKeys);
+    KeysJson.body = JSON.stringify(jFLocalserializeFormData(jVarLocalForm));
 
     return KeysJson;
+};
+
+function jFLocalserializeFormData(form) {
+    var formData = new FormData(form);
+    var serializedData = {};
+
+    for (var [name, value] of formData) {
+        if (serializedData[name]) {
+            if (!Array.isArray(serializedData[name])) {
+                serializedData[name] = [serializedData[name]];
+            }
+            serializedData[name].push(value);
+        } else {
+            serializedData[name] = value;
+        }
+    }
+
+    return serializedData;
 };
 
 export { StartFunc }
--- srcByPath/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-var router = express.Router();
-
-import { router{{ksSample}} } from './{{ksSample}}/{{ksSample}}Route.js';
-
-router.use('/{{ksSample}}', router{{ksSample}});
-
-export { router };
