1bdb812263fef71cc38b5b182dade6d94e16d5b2
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-23 13:02:05 +0530

    SREENIVAS

--- public/JsonCRUD/Custom/Shasank/Alter/js/formLoad/pullTableData/AfterFetch/AfterFetch.js
+++ public/JsonCRUD/Custom/Shasank/Alter/js/formLoad/pullTableData/AfterFetch/AfterFetch.js
@@ -1,22 +1,6 @@
+import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 let StartFunc = ({ inFromFetch }) => {
-    jFLocalToInnerHtmlid({ inid: inFromFetch.id })
-    jFLocalToInputJobId({ inid: inFromFetch.JobId })
-
-};
-
-let jFLocalToInnerHtmlid = ({ inid }) => {
-    let jVarLocalHtmlId = 'Id';
-    let jVarLocalid = document.getElementById(jVarLocalHtmlId);
-    jVarLocalid.value = inid;
-};
-
-let jFLocalToInputJobId = ({ inJobId }) => {
-    let jVarLocalHtmlId = 'JobId';
-   let jVarLocalJobId = document.getElementById(jVarLocalHtmlId);
-   
-   if (jVarLocalJobId === null === false) {
-      jVarLocalJobId.value = inJobId;
-   };
+    StartFuncShowOnDom({ inFromFetch });
 };
 
 
