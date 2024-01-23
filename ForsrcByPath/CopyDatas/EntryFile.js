6b3fe6f5185df610268a038caa98dc882f695d52
Keshav <nknnkumar@yahoo.com>
2024-01-23 17:01:25 +0530

    keshav

--- ForsrcByPath/CopyDatas/EntryFile.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import ToConfigJson from '../../srcByPath/ToConfig.json' assert { type: 'json' };
-
-let StartFunc = ({ inFromFolderName }) => {
-    let LocalFromFolderName = inFromFolderName;
-    fs.cpSync(`${LocalFromFolderName}`, ToConfigJson.JsonPath, { recursive: true });
-};
-
-export { StartFunc };
--- kStudy/read.js
+++
@@ -1,4 +0,0 @@
-import { ClassMates } from "./kclass1.js";
-let classmate = new ClassMates("Mike Will", 15);
-
-console.log("aaaaaaa : ", classmate.mobile);
