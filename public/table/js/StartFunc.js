1d923efd7d018a31c3956e67d8ee734c2381f70b
Keshav <nknnkumar@yahoo.com>
2024-01-24 12:01:43 +0530

    keshav

--- public/table/js/StartFunc.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncNavMenu } from "./NavMenu/EntryFile.js";
-import { StartFunc as StartFuncformLoad } from "./formLoad/pullTableData/ButtonClickFunc.js";
-
-const StartFunc = () => {
-    StartFuncNavMenu();
-    StartFuncformLoad().then();
-};
-
-StartFunc();
