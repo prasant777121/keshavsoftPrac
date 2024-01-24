ba9b03492d452849fc619c197c6515dc8b1efb00
Keshav <nknnkumar@yahoo.com>
2024-01-24 10:24:40 +0530

    keshav

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(express.json({ limit: '100mb' }));
 
-app.use('/', express.static(path.join(path.resolve(), 'public')))
+app.use('/', express.static(path.join(path.resolve(), 'public')));
 
 app.get('/AboutUs', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- public/JsonCRUD/src/ksSample/BuildTh/Table-/js/formLoad/Addlisteners/StartFunc.js
+++
@@ -1,7 +0,0 @@
-import { StartFunc as StartFuncSelectedColumns } from "./RefreshButtonClick/StartFunc.js";
-
-let StartFunc = () => {
-    StartFuncSelectedColumns();
-};
-
-export { StartFunc };
