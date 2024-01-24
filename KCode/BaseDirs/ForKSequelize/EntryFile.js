be3ed5e593cb630c197671c24b8ac986d37d9b6e
Keshav <nknnkumar@yahoo.com>
2024-01-24 15:23:39 +0530

    keshav

--- KCode/BaseDirs/ForKSequelize/EntryFile.js
+++ KCode/BaseDirs/ForKSequelize/EntryFile.js
@@ -8,11 +8,11 @@ let StartFunc = () => {
 
         fs.cpSync(`${LocalFrom}/${LocalFolderName}`, `${LocalTo}/${LocalFolderName}`, { recursive: true });
 
-        let LocalFileData = fs.readFileSync(`${LocalTo}/${LocalFolderName}/Gets.http`);
+        let LocalFileData = fs.readFileSync(`${LocalTo}/${LocalFolderName}/gets.http`);
 
         let LocalNewFileData = LocalFileData.toString().replaceAll(LocalFrom, LocalTo);
 
-        fs.writeFileSync(`${LocalTo}/${LocalFolderName}/Gets.http`, LocalNewFileData);
+        fs.writeFileSync(`${LocalTo}/${LocalFolderName}/gets.http`, LocalNewFileData);
     } catch (error) {
         console.log("error  : ", error);
     };
