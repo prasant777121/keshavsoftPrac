4a7e04e68ec6cddefc489ca89a870229f3b0f484
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-29 18:10:44 +0530

    SREEENIVAS

---
+++ public/JsonCRUD/Custom/Shasank/Technician-Menu/Inputs/js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -0,0 +1,5 @@
+let StartFunc = () => {
+    window.location.href = "../Status/Status.html";
+};
+
+export { StartFunc }
---
+++ public/JsonCRUD/Custom/Shasank/Technician-Menu/Inputs/js/formLoad/pullTableData/AfterFetch/AfterFetch.js
@@ -0,0 +1,7 @@
+import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
+let StartFunc = ({ inFromFetch,InTechnicianData }) => {
+    StartFuncShowOnDom({ inFromFetch,InTechnicianData });
+};
+
+
+export { StartFunc };
