ca2afbc2ad0990c277d31812ba759e1294ff239a
Keshav <nknnkumar@yahoo.com>
2024-01-30 15:00:51 +0530

    keshav

--- src/ksSample/dals/postFuncs/UsingMulter.js
+++ src/ksSample/dals/postFuncs/UsingMulter.js
@@ -1,12 +1,16 @@
 import multer from 'multer';
-import multer from '../../../Config.json';
+import ConfigJson from '../../../Config.json' assert {type: 'json'};
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        
+        let LocalUrl=req.originalUrl;
+        let LocalUrlArray=LocalUrl.split("/");
+        console.log("aaaaaaaaa : ", req.originalUrl,LocalUrlArray);
         cb(null, "uploads");
     },
     filename: function (req, file, cb) {
-        let LocalId=req.params.Id;
+        let LocalId = req.params.Id;
         console.log(LocalId);
         cb(null, `${LocalId}.jpg`);
     }
